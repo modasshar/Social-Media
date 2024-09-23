5ed1792bddfcefbd26d4d0465a7b28e2c26d0688
modasshar <modasshar.hussain@gmail.com>
2024-09-23 21:35:57 +0530

    adding files

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,71 @@
+import { createContext, useContext, useReducer } from "react";
+export const PostList = createContext({
+    postList: [],
+    addPost: () => {},
+    deletePost: () => {},
+})
+
+const postListReducer = (currPostList , action) =>{
+    let newPostList = currPostList ;
+    if(action.type === 'DELETE_POST'){
+        newPostList = currPostList.filter(post => post.id !== action.payload.postId);
+    } else if (action.type === "ADD_POST") {
+        newPostList = [action.payload, ...currPostList];
+      }
+    return newPostList
+}
+
+const PostListProvider =  ({children}) => {
+    const [postList, dispatchPostList] = useReducer(postListReducer ,DEFAULT_POST_LIST)
+    const addPost = (userId ,postTitle, postBody, reactions, tags) => {
+        dispatchPostList({
+            type: 'ADD_POST', 
+            payload: {
+                id: Date.now(),
+                title: postTitle ,
+                body: postBody,
+                reactions: reactions,
+                userId: userId,
+                tags: tags ,
+            },
+        })
+    }
+    const deletePost = (postId) => {
+        dispatchPostList(
+            {
+                type: "DELETE_POST",
+                payload:{
+                    postId, 
+                },
+            }
+        )
+    }
+
+    return (
+        <PostList.Provider value={{ postList, addPost, deletePost }}>
+          {children}
+        </PostList.Provider>
+      );
+}
+
+const DEFAULT_POST_LIST  = [
+    {
+        id: '1',
+        title: 'Going to Mumbai' ,
+        body: 'Hi friends, I am going to Mumbai for myu vacations, Hope to enjoye a lot , Peace out.',
+        reactions: 2,
+        userId: 'user-9',
+        tags: ["vacation", "Mumbai", "Enjoying"] ,
+    },
+    {
+        id: '2',
+        title: 'Pass ho bhai' ,
+        body: '4 saal ki masti ke baad bhi ho gaye hum pass. Hard to blieve.',
+        reactions: 15,
+        userId: 'user-12',
+        tags: ["Graduating", "Unblievable"] 
+    },
+    
+]
+
+export default PostListProvider 
